8b66a645a7825af4107573521c9821af15d6d3b2
Zilehuda <Zilehuda@gmail.com>
2016-11-20 07:52:33 -0800

    login page created with php

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+$serverName = "HP"; //serverName\instanceName
+
+// Since UID and PWD are not specified in the $connectionInfo array,
+// The connection will be attempted using Windows Authentication.
+$connectionInfo = array( "Database"=>"STP");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     //echo "Connection established.<br />";
+
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
